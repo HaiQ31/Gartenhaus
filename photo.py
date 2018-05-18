bc2f26dd330a9e151af94fcd8a50df81a911be51
HaiQ31 <39369706+HaiQ31@users.noreply.github.com>
2018-05-18 17:47:47 +0200

    Update photo.py
    mcp3008 channel auslesen als Funktion ausgelagert

--- photo.py
+++ photo.py
@@ -2,6 +2,7 @@ import spidev
 import time
 import os
 import RPi.GPIO as gpio
+from mcp3008 import mcp
 
 # SPI Verbindung herstellen
 spi = spidev.SpiDev()
@@ -10,15 +11,15 @@ spi.max_speed_hz = 1000000
 
 # Liest Daten vom MCP3008
 
-def mcp3008(channel):
- adc = spi.xfer2([1,(8+channel)<<4,0])
-# print(adc)
- data = ((adc[1]&3) << 8) + adc[2]
-# print(data)
- return data
+#def mcp3008(channel):
+# adc = spi.xfer2([1,(8+channel)<<4,0])
+## print(adc)
+# data = ((adc[1]&3) << 8) + adc[2]
+## print(data)
+# return data
 
 while True:
-  wert = mcp3008(0)
+  wert = mcp(0)
 #  print("wert: ", wert)
   print("0: "+str(analogEingang(0)))
   time.sleep(1)
